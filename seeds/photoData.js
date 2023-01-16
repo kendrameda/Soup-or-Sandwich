c4e97acae07cfb6585b69fd9ab103071f59e7372
wdiste <Wdiste49@gmail.com>
2023-01-16 14:06:51 -0500

    attempted to make cloudinary work.  Unsuccessful

--- seeds/photoData.js
+++ seeds/photoData.js
@@ -1,8 +1,9 @@
+
 const photoData =
 [
     {
         type: "soup",
-        image: "link"
+        image: "https://res.cloudinary.com/dijep6dqz/image/upload/v1673890632/sample.jpg"
     },
     {
         type: "soup",
