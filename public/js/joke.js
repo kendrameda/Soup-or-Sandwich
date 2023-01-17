b32e91c115cc1cc0a3b9972767968644dddfd562
wdiste <Wdiste49@gmail.com>
2023-01-17 14:23:12 -0500

    added background to joke page

--- public/js/joke.js
+++ public/js/joke.js
@@ -5,12 +5,4 @@ startBttn.addEventListener("click", function(event) {
     console.log(event);
     console.log(event.target);
     window.location.replace("localhost:3001/login");
-})
-
-
-const bttn = document.querySelector("#button");
-
-bttn.addEventListener('click', function() {
-    console.log('button pressed');
-    alert("button pressed");
-})
+});
